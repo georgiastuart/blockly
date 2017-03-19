0c27ee51bfbee11d78c3a2c291aacbe26097e866
Georgia Stuart <georgia.stuart@gmail.com>
2017-03-18 21:34:08 -0500

    implement innovator_io_led and innovator_io_led_ext

--- blocks/innovator_io.js
+++ blocks/innovator_io.js
@@ -7,11 +7,43 @@ goog.provide('Blockly.Blocks.innovator_io');
 
 goog.require('Blockly.Blocks');
 
+Blockly.Blocks['innovator_io_led_ext'] = {
+    init: function () {
+        this.jsonInit({
+          "type": "innovator_io_led_ext",
+          "message0": "Turn the RED Led %1 for %2 seconds",
+          "args0": [
+            {
+              "type": "field_dropdown",
+              "name": "STATE",
+              "options": [
+                [
+                  "On",
+                  "ON"
+                ],
+                [
+                  "Off",
+                  "OFF"
+                ]
+              ]
+            },
+            {
+              "type": "input_value",
+              "name": "TIME",
+              "check": "Number"
+            }
+          ],
+          "inputsInline": true,
+          "previousStatement": null,
+          "nextStatement": null,
+          "colour": 58,
+          "tooltip": "",
+          "helpUrl": ""
+        });
+    }
+};
+
 Blockly.Blocks['innovator_io_led'] = {
-    /**
-     * Block for print statement.
-     * @this Blockly.Block
-     */
     init: function () {
         this.jsonInit({
             "type": "innovator_io_led",
@@ -19,7 +51,7 @@ Blockly.Blocks['innovator_io_led'] = {
             "args0": [
             {
               "type": "field_dropdown",
-              "name": "NAME",
+              "name": "STATE",
               "options": [
                 [
                   "On",
@@ -32,9 +64,11 @@ Blockly.Blocks['innovator_io_led'] = {
               ]
             }
             ],
-            "colour": 230,
+            "colour": 58,
             "tooltip": "",
-            "helpUrl": ""
+            "helpUrl": "",
+            "previousStatement": null,
+            "nextStatement": null
         });
     }
 };
--- generators/tibasic/innovator_io.js
+++ generators/tibasic/innovator_io.js
@@ -8,9 +8,25 @@ goog.provide('Blockly.TIBasic.innovator_io');
 
 goog.require('Blockly.TIBasic');
 
-Blockly.JavaScript['innovator_io_led'] = function(block) {
-  var dropdown_name = block.getFieldValue('NAME');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+Blockly.TIBasic['innovator_io_led_ext'] = function(block) {
+    var state = block.getFieldValue('STATE');
+    if (block.getField('TIME')) {
+        // Internal number.
+        var time = String(Number(block.getFieldValue('TIME')));
+    } else {
+        // External number.
+        var time = Blockly.TIBasic.valueToCode(block, 'TIME',
+        Blockly.TIBasic.ORDER_ASSIGNMENT) || '0';
+        if (!Blockly.isNumber(time)) {
+            time = 'eval(' + String(time) + ')'
+        }
+    }
+    var code = 'Send("SET LIGHT ' + state + ' TIME ' + time + '"):';
+    return code;
+};
+
+Blockly.TIBasic['innovator_io_led'] = function(block) {
+  var state = block.getFieldValue('STATE');
+  var code = 'Send("SET LIGHT ' + state + '"):';
   return code;
 };
